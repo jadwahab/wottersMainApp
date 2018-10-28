47f98e5a739bb4c89f1b13e130fd081d161f5113
Bartel Verkruijssen <bartel@verkruijssen.com>
2018-10-28 12:51:03 +0100

    interface change

--- src/viewImages.js
+++ src/viewImages.js
@@ -5,37 +5,12 @@ import StackGrid, { transitions } from "react-stack-grid";
 // import fetch from 'node-fetch'
 const { scaleDown } = transitions;
 
-const data = [
-  {
-      "hash": "7cbb",
-      "pubKey": "0x45623423",
-      "assignment": "White hat, green sweater, devil sign",
-      "prevHash": "9ee3",
-      "imageHash": "AAAA",
-      "timeStamp": "2018-01-01 12:31"
-  }, 
-  {
-      "hash": "8a34",
-      "pubKey": "0x34524543",
-      "assignment": "Green hat, red sweater, peace sign",
-      "prevHash": "9ee3",
-      "imageHash": "4805_110886908573_8281698_n",
-      "timeStamp": "2018-01-01 12:30"
-  }, 
-  {
-      "hash": "9ee3",
-      "pubKey": "0x34452834",
-      "assignment": "Blue hat, white sweater, o sign",
-      "prevHash": "6e3b",
-      "imageHash": "582069_10151283054588574_1283163895_n",
-      "timeStamp": "2018-01-01 12:12"
-  }
-]
-
 
 class ViewImages extends Component {
-
     state = {
+      a: {display: ''},
+      b: {display: ''},
+      c: {display: ''},
       items: [
         {
           hash: '7cbb',
@@ -67,59 +42,38 @@ class ViewImages extends Component {
       ]
   }
 
-  onYes() {
-    console.log('click yes')
+  onYes(hash) {
+    console.log('clicked')
+    window.open('/users','_self')
   }
 
   onNo() {
     console.log('click no')
   }
 
+
   render() {
     const { items } = this.state
-    // console.log(items)
+    console.log(items)
     return (
       <div className="App">
-        <StackGrid
-          columnWidth={300}
-          appear={scaleDown.appear}
-          appeared={scaleDown.appeared}
-          enter={scaleDown.enter}
-          entered={scaleDown.entered}
-          leaved={scaleDown.leaved}
-        >
-        {
-          items.map(({ hash, pubKey, assignment,prevHash, imageHash, timeStamp }) => {
-            const status = {display: 'none'} 
-            return (
-              // <Row>
-              //   <Col >
-              
-                <div onClick={() => {
-                  console.log('you click')
-                  this.setState({
-                    items: items.map(o => ({
-                      ...o,
-                      active: o.hash === hash ? !o.active : o.active,
-                    })),
-                  });
-                }}  key={hash} style={{marginButton: '2%', status}} className="boxes">
+              <Row>
+                <Col xs="6" sm="4"></Col>
+                <Col >
+                <div key={items[0].hash} style={items[0].statusThing}>
                 <div className="card" style={{width: '18rem'}}>
-                  <img className="card-img-top" src={`https://wotters.blob.core.windows.net/images/${imageHash}.jpg`} alt="Card image cap" />
+                  <img className="card-img-top" src={`https://wotters.blob.core.windows.net/images/${items[0].imageHash}.jpg`} alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Challenge</h5>
-                    <p className="card-text">{assignment}</p>
-                    <Button block onClick={() => this.onYes()} style={{marginRight: '30px'}} color="success">Yes</Button> 
+                    <p className="card-text">{items[0].assignment}</p>
+                    <Button block onClick={() => this.onYes(items[0].hash)} style={{marginRight: '30px'}} color="success">Yes</Button> 
                     <Button block onClick={() => this.onNo()} color="danger">No</Button>
                   </div>
                 </div>
               </div>
-            //   </Col>
-            // </Row>
-            )
-          })
-        }   
-        </StackGrid>
+              </Col>
+              <Col xs="6" sm="4"></Col>
+            </Row>
       </div>
     );
   }
